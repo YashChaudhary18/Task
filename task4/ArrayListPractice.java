b6fea75864f48600c697220b47452375914a8ad5
YashChaudhary18 <yashchaudhary@t7solution.com>
2026-03-17 15:38:00 +0530

    Add files via upload

---
+++ task4/ArrayListPractice.java
@@ -0,0 +1,50 @@
+package task4;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListPractice {
+
+    public static void main(String[] args) {
+
+        // Create ArrayList
+        ArrayList<String> names = new ArrayList<>();
+
+        // Store 5 student names
+        names.add("Yash");
+        names.add("Rahul");
+        names.add("Amit");
+        names.add("Neha");
+        names.add("Priya");
+
+        // Print all names
+        System.out.println("Student Names:");
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i));
+        }
+
+        // Take input from user
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nEnter name to search: ");
+        String search = sc.nextLine();
+
+        // Search name
+        boolean found = false;
+
+        for (int i = 0; i < names.size(); i++) {
+            if (names.get(i).equals(search)) {
+                found = true;
+                break;
+            }
+        }
+
+        // Display result
+        if (found) {
+            System.out.println("Name found in the list");
+        } else {
+            System.out.println("Name not found in the list");
+        }
+
+        sc.close();
+    }
+}
